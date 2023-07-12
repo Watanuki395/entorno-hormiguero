8771e80f1a08bd2187a6e0c31e64c3bb469c2cde
Gerardo Adolfo Salas Montoya <43663+gesalas@users.noreply.github.kyndryl.net>
2023-07-12 11:13:49 -0600

    adding update to app port

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
+const http = require('http')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Entorno creado...!')
 })
 
 app.listen(port, () => {
